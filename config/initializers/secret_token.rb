fbd2bc0a6e44eaa0941bdf74799ffd4af93ca7b3
flenoir <flenoir@gmail.com>
2014-10-28 10:47:17 +0100

    initial commit projet Djobs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Djobs::Application.config.secret_key_base = 'c5a9b516b73ae4b29aaeaadb2602ae23743f86be0190440ea459e3b7953c47ea6da94320f1c1b3e6475691b2fb9f10de0e6a183113dbdd619464c0e1c47a000c'
